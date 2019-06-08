88df9d1e595b389b018c466927be3575fbcac7dd
ray-z <ray-z15@hotmail.com>
2019-06-08 12:23:40 +0200

    Adivina un numero con un intento

---
+++ adivina_el_numero.py
@@ -0,0 +1,7 @@
+num_correcto = 7
+num_elegido = int(input("elige de un numero del 1 al 10: "))
+
+if num_correcto == num_elegido:
+    print("adivinaste")
+else:
+    print("perdiste intenta de nuevo")
