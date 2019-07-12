3648e1e19e4f60fec4c7785dd54f48818f4d9269
ray-z <ray-z15@hotmail.com>
2019-07-12 18:01:12 +0200

    Terminado

---
+++ grados_farenheit_a_celcius.py
@@ -0,0 +1,4 @@
+grados_farenheit = float(input("¿Cuantos grados Farenheit quieres transformar en Celcius?: "))
+grados_celcius = (grados_farenheit - 32) / 1.8
+
+print("{} Grados Celcius".format(grados_celcius))
