325b863c47fd0da459241fd6fcbcefae2016a06c
ray-z <ray-z15@hotmail.com>
2019-07-15 16:35:32 +0200

    tabla de multiplicar del 5 al 15

--- tabla_de_multiplicar.py
+++ tabla_de_multiplicar.py
@@ -1,5 +1,5 @@
 num_tabla = int(input("Escibe el numero del que quieres ver su tabla de multiplicar hasta el 10: "))
 
-for multiplo in range(1, 11):
+for multiplo in range(5, 16):
     print("{} x {} = {}".format(num_tabla, multiplo, num_tabla * multiplo))
 
