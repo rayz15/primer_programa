63fc150772004786b2c51046661cc45c2d7b2c8f
ray-z <ray-z15@hotmail.com>
2019-07-16 16:08:56 +0200

    tabla de multiplicar invertida

--- tabla_de_multiplicar.py
+++ tabla_de_multiplicar.py
@@ -1,5 +1,4 @@
 num_tabla = int(input("Escibe el numero del que quieres ver su tabla de multiplicar hasta el 10: "))
-for multiplo in range(1, 16):
-    if  multiplo % 2 == 0:
-        print("{} x {} = {}".format(num_tabla, multiplo, num_tabla * multiplo))
 
+for multiplo in reversed(range(1, 11)):
+    print("{} x {} = {}".format(num_tabla, multiplo, num_tabla * multiplo))
