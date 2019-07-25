828dd079f1f61a19f31c06dc94f3e28069c09201
ray-z <ray-z15@hotmail.com>
2019-07-25 17:00:46 +0200

    lista de largo de cada string en la lista

---
+++ lista_tipos_2.py
@@ -0,0 +1,7 @@
+lista_datos = ["tu mama", "perro", "pistola", "futuro", "destino", "muerte", "rata", "vaca"]
+lista_strings = []
+
+for string in lista_datos:
+    lista_strings.append(len(string))
+    print(lista_strings)
+
