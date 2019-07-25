3b8082adfbac23de42bf181c7616f81c71bfaa3e
ray-z <ray-z15@hotmail.com>
2019-07-25 16:49:15 +0200

    media de lista de numeros

--- numero_mas_grande.py
+++ numero_mas_grande.py
@@ -1,17 +1,13 @@
 numeros_usuario = []
-numeros_del_usuario = ""
+numero_del_usuario = ""
 
 while len(numeros_usuario) < 10:
-    while not numeros_del_usuario.isdigit():
-        numeros_del_usuario = input("Escribe un numero: ")
-    numeros_usuario.append(int(numeros_del_usuario))
-    numeros_del_usuario = ""
+    while not numero_del_usuario.isdigit():
+        numero_del_usuario = input("Dime un numero: ")
+    numeros_usuario.append(float(numero_del_usuario))
+    numero_del_usuario = ""
     print("Numero añadido")
 
-numero_pequeno = numeros_usuario[0]
-for numero in numeros_usuario:
-    if numero < numero_pequeno:
-        numero_pequeno = numero
-
-print("El numero menor es {}".format(numero_pequeno))
-
+print(numeros_usuario)
+media = (numeros_usuario [0] + numeros_usuario [1] + numeros_usuario [2] + numeros_usuario [3] + numeros_usuario [4] + numeros_usuario [5] + numeros_usuario [6] + numeros_usuario [7] + numeros_usuario [8] + numeros_usuario [9]) / 10
+print(media)
