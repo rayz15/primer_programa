b856fa155ed5be50040aaac5d3f2fa524bdf0467
ray-z <ray-z15@hotmail.com>
2019-07-25 17:09:56 +0200

    lista de la mutiplicacion de los numeros de la lista

---
+++ lista_tipos_3.py
@@ -0,0 +1,4 @@
+lista_numeros = [2, 3, 5, 6, 8, 2, 7]
+multiplicacion = (lista_numeros [0] * lista_numeros [1] * lista_numeros [2] * lista_numeros [3] * lista_numeros [4] * lista_numeros [5] * lista_numeros [6])
+print(lista_numeros)
+print((multiplicacion))
